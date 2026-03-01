12591c7da9edd59f1360d38baac9b57b21130d92
Diego Paiva <jose.diego@tjce.jus.br>
2026-02-28 22:36:26 -0300

    feat: correção de sign_out

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -4,6 +4,8 @@ module Api
       class SessionsController < Devise::SessionsController
         respond_to :json
 
+        skip_before_action :verify_signed_out_user, only: :destroy
+
         private
 
         def respond_with(resource, _opts = {})
@@ -20,7 +22,7 @@ module Api
           }, status: :ok
         end
 
-        def respond_to_on_destroy
+        def respond_to_on_destroy(non_navigational_status: :no_content)
           head :no_content
         end
       end
