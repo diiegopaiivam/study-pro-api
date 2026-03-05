1b520cd303c105a40f8ec03a39ec65e853253be5
Diego Paiva <jose.diego@tjce.jus.br>
2026-03-04 23:30:51 -0300

    feat: ajustando arquivo de deploy

--- config/initializers/rails_pulse.rb
+++ config/initializers/rails_pulse.rb
@@ -136,7 +136,7 @@ RailsPulse.configure do |config|
   # Uncomment and configure one of the following patterns based on your authentication system:
 
   # Enable/disable authentication (enabled by default in production)
-  config.authentication_enabled = true
+  config.authentication_enabled = false
 
   # Where to redirect unauthorized users
   #config.authentication_redirect_path = "/login"
